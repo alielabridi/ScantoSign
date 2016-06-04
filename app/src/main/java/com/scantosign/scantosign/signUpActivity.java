8b22e9ae211121a0e8c557fafb03c1e3ec459fee
alielabridi <elabridiali@outlook.com>
2016-06-04 18:42:52 +0100

    set up the android project, added the sign up form, modified the icon

---
+++ app/src/main/java/com/scantosign/scantosign/signUpActivity.java
@@ -0,0 +1,241 @@
+package com.scantosign.scantosign;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class signUpActivity extends AppCompatActivity{
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private EditText EmailView;
+    private EditText StudentIDView;
+    private EditText FirstNameView;
+    private EditText LastnameView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        setupActionBar();
+
+
+        EmailView = (EditText) findViewById(R.id.email);
+        //FirstNameView = (EditText) findViewById(R.id.firstname);
+        //LastnameView = (EditText) findViewById(R.id.lastname);
+        //StudentIDView = (EditText) findViewById(R.id.studentid);
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptToSignUp();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setupActionBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptToSignUp() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        EmailView.setError(null);
+/*        LastnameView.setError(null);
+        FirstNameView.setError(null);
+        StudentIDView.setError(null);*/
+
+        // Store values at the time of the login attempt.
+        String email = EmailView.getText().toString();
+        //String firstname = FirstNameView.getText().toString();
+        //String lastname = LastnameView.getText().toString();
+        //String studentid = StudentIDView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+/*
+
+        // Check for a valid lastname, if the user entered one.
+        if (!TextUtils.isEmpty(lastname)) {
+            LastnameView.setError(getString(R.string.error_empty_field));
+            focusView = LastnameView;
+            cancel = true;
+        }
+
+        // Check for a valid firstname, if the user entered one.
+        if (!TextUtils.isEmpty(firstname)) {
+            FirstNameView.setError(getString(R.string.error_empty_field));
+            focusView = FirstNameView;
+            cancel = true;
+        }
+        // Check for a valid studentid, if the user entered one.
+        if (!TextUtils.isEmpty(studentid)) {
+            StudentIDView.setError(getString(R.string.error_empty_field));
+            focusView = StudentIDView;
+            cancel = true;
+        }
+
+*/
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            EmailView.setError(getString(R.string.error_empty_field));
+            focusView = EmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            EmailView.setError(getString(R.string.error_invalid_email));
+            focusView = EmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            //mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
+}
+
