53c2bdb357102ac8388c3640744be15182775ac2
alielabridi <elabridiali@outlook.com>
2016-06-05 18:25:01 +0000

    commit to save

---
+++ app/src/main/java/com/scantosign/scantosign/QRCodeScan.java
@@ -0,0 +1,54 @@
+package com.scantosign.scantosign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class QRCodeScan extends AppCompatActivity implements OnQRCodeReadListener {{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_scan);
+
+        mydecoderview = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        mydecoderview.setOnQRCodeReadListener(this);
+
+        myTextView = (TextView) findViewById(R.id.exampleTextView);
+    }
+
+
+    // Called when a QR is decoded
+    // "text" : the text encoded in QR
+    // "points" : points where QR control points are placed
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        myTextView.setText(text);
+    }
+
+
+    // Called when your device have no camera
+    @Override
+    public void cameraNotFound() {
+
+    }
+
+    // Called when there's no QR codes in the camera preview image
+    @Override
+    public void QRCodeNotFoundOnCamImage() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mydecoderview.getCameraManager().startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mydecoderview.getCameraManager().stopPreview();
+    }
+}
+}
