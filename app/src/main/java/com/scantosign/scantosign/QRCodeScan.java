033c082b7744f787df953a6b392aa2ed141f4300
alielabridi <elabridiali@outlook.com>
2016-09-05 16:39:46 +0100

    Revert "Found a potential patch for the camera flip"
    This reverts commit 114d5f57e0128c0011960aab5123a536112d499a.

--- app/src/main/java/com/scantosign/scantosign/QRCodeScan.java
+++ app/src/main/java/com/scantosign/scantosign/QRCodeScan.java
@@ -1,12 +1,9 @@
 package com.scantosign.scantosign;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.PointF;
-import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Surface;
 import android.widget.TextView;
 
 
@@ -20,53 +17,9 @@ public class QRCodeScan extends AppCompatActivity implements OnQRCodeReadListene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode_scan);
 
-        //fixing camera rotating in some devices
-        int findFrontFacingCameraID = findFrontFacingCameraID();
-        setCameraDisplayOrientation(QRCodeScan.this,findFrontFacingCameraID,Camera.open(findFrontFacingCameraID));
-
         mydecoderview = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
         mydecoderview.setOnQRCodeReadListener(this);
 
-    }
-
-    private int findFrontFacingCameraID() {
-        int cameraId = -1;
-        // Search for the front facing camera
-        int numberOfCameras = Camera.getNumberOfCameras();
-        for (int i = 0; i < numberOfCameras; i++) {
-            Camera.CameraInfo info = new Camera.CameraInfo();
-            Camera.getCameraInfo(i, info);
-            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-                cameraId = i;
-                break;
-            }
-        }
-        return cameraId;
-    }
-
-    public static void setCameraDisplayOrientation(Activity activity,
-                                                   int cameraId, android.hardware.Camera camera) {
-        android.hardware.Camera.CameraInfo info =
-                new android.hardware.Camera.CameraInfo();
-        android.hardware.Camera.getCameraInfo(cameraId, info);
-        int rotation = activity.getWindowManager().getDefaultDisplay()
-                .getRotation();
-        int degrees = 0;
-        switch (rotation) {
-            case Surface.ROTATION_0: degrees = 0; break;
-            case Surface.ROTATION_90: degrees = 90; break;
-            case Surface.ROTATION_180: degrees = 180; break;
-            case Surface.ROTATION_270: degrees = 270; break;
-        }
-
-        int result;
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-            result = (info.orientation + degrees) % 360;
-            result = (360 - result) % 360;  // compensate the mirror
-        } else {  // back-facing
-            result = (info.orientation - degrees + 360) % 360;
-        }
-        camera.setDisplayOrientation(result);
     }
 
 
